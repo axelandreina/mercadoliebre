b0f01d79c2cab9f86cad3d3461c1dfbe39af70e0
axelandreina <axelandre91@gmail.com>
2021-05-01 20:34:10 -0300

    created procfile

--- app.js
+++ app.js
@@ -17,6 +17,6 @@ app.get('/register', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/register.html'))
 })
 
-app.listen(3080, () => {
+app.listen(process.env.PORT || 3080, () => {
     console.log('Servidor andando en el puerto 3080');
 })
